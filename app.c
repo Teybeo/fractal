9bc2b45f8fd5e239e8b96417424afc11bbe3d2e1
Thibault D'ARCHIVIO <tdarchiv@student.42.fr>
2018-12-12 14:05:53 +0100

    Initial commit

---
+++ app.c
@@ -0,0 +1,8 @@
+#include "app.h"
+
+#include <mlx.h>
+
+t_app	app_init()
+{
+	mlx_init();
+}
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "app.h"
+
+int main()
+{
+	t_app app;
+
+	app = app_init();
+//	app_run();
+	return 0;
+}
